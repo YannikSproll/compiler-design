b0adc1775bbe2f5685a6b4ebae2910ede2d230d3
ukgsr <ukgsr@student.kit.edu>
2025-06-11 14:55:52 +0200

    Removed deprecated node sequence analysis.

--- src/main/java/edu/kit/kastel/vads/compiler/backend/aasm/NodeSequenceAnalysis.java
+++
@@ -1,7 +0,0 @@
-package edu.kit.kastel.vads.compiler.backend.aasm;
-
-import edu.kit.kastel.vads.compiler.ir.IrGraph;
-
-public interface NodeSequenceAnalysis {
-    NodeSequence sequenceNodes(IrGraph graph);
-}
