f59439c969204954a05ccbe349486e2e6c8502a0
ukgsr <ukgsr@student.kit.edu>
2025-06-15 11:58:13 +0200

    Added missing modification of typed loop to support single statement bodys.

--- src/main/java/edu/kit/kastel/vads/compiler/frontend/semantic/hir/TypedLoop.java
+++ src/main/java/edu/kit/kastel/vads/compiler/frontend/semantic/hir/TypedLoop.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 
 public record TypedLoop(
         TypedExpression conditionExpression,
-        TypedBlock body,
+        TypedStatement body,
         Optional<TypedStatement> postIterationStatement,
         Span span) implements TypedStatement {
     @Override
